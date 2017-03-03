e1bc7dc75a7e3efff4b85670777d0fea41816544
corneyy <corneyy@gmail.com>
2017-03-03 20:22:39 +0400

    FIX The label is not mandatory in the media tag

--- converter/lens_converter.js
+++ converter/lens_converter.js
@@ -1867,14 +1867,18 @@ NlmToLensConverter.Prototype = function() {
 
   this.video = function(state, video) {
     var doc = state.doc;
-    var label = video.querySelector("label").textContent;
+
+    var label = video.querySelector("label");
+    var labelText = label
+      ?label.textContent
+      :'';
 
     var id = state.nextId("video");
     var videoNode = {
       "id": id,
       "source_id": video.getAttribute("id"),
       "type": "video",
-      "label": label,
+      "label": labelText,
       "title": "",
       "caption": null,
       "poster": ""

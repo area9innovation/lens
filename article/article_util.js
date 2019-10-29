17b68e41ed6a32123d3441fd2c28189f40aa9b0d
Andrey Tkachenko <andrey.tkachenko@area9.dk>
2019-10-29 14:35:20 +0200

    fix to comply with ORCID ID format

--- article/article_util.js
+++ article/article_util.js
@@ -63,7 +63,7 @@ util.monthSymToNum = function (sym){
 util.extractOrcidId = function(orcid_url) {
     // http://orcid.org/0000-0002-8808-1137
     var orcid_id = orcid_url;
-    var matches = orcid_url.match(/[\d]{4}-[\d]{4}-[\d]{4}-[\d]{4}/);
+    var matches = orcid_url.match(/[\d]{4}-[\d]{4}-[\d]{4}-[\d]{3}[\dX]{1}/);
     if (matches && matches.length) {
       orcid_id = matches[0];
     }

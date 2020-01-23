0451fbaeeba8e749d9635c900b366331f1c49abf
Andrey Tkachenko <andrey.tkachenko@area9.dk>
2020-01-23 13:27:22 +0200

    avoid crash when cross reference not found

--- reader/workflows/follow_crossrefs.js
+++ reader/workflows/follow_crossrefs.js
@@ -24,7 +24,10 @@ FollowCrossrefs.Prototype = function() {
     e.stopPropagation();
     var refId = e.currentTarget.dataset.id;
     var crossRef = this.readerCtrl.getDocument().get(refId);
-    
+
+    if (!crossRef) {
+      return;
+    }
     if( this.readerView.contentView.findNodeView(crossRef.target) ) {
       this.readerView.contentView.scrollTo(crossRef.target);
     } else if( this.readerView.panelViews.info.findNodeView(crossRef.target) ) {
